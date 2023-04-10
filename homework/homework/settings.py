e6f449237a810db29900ec34e440158b4d743752
mhussam2000 <115428684+mhussam2000@users.noreply.github.com>
2023-04-11 02:32:45 +0500

    django connected with postgre

--- homework/homework/settings.py
+++ homework/homework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dd1mc5jbw-1m-i_2hll0=9m!1j&1ne03ca@6!t8^@n4zlv%_aa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['*']
 
 
 # Application definition
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'homework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'jawad021',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
